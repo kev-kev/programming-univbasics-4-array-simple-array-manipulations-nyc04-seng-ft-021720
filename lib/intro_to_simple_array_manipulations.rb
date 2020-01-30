cff1db10095ce2c0a4ba271815ef5c0d05a1e241
Kevin Clark <kevin.clark93@gmail.com>
2020-01-30 20:34:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,10 @@ def shift_with_args (array)
   array.shift(2)
 end
 
-def using_concat (array, element)
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert (array, element)
   array.insert(4, element)
 end
